2fdb33cc52805a2cf881150f6d9b0d6bd9078dfc
Stefan <37850379+pomber12@users.noreply.github.com>
2021-05-14 10:44:02 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+
+import React from "react";
+import "./App.css";
+
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+import {Link} from "react-router-dom"
+
+
+
+import {Route,Switch} from 'react-router-dom';
+
+import Navbar from "./components/Navbar";
+
+
+
+function App() {
+  return (
+   
+    
+    <>
+
+    <Navbar />
+    <Switch>
+    
+
+    <Route exact path="/Home" component={Home}/>
+    <Route exact path="/rooms" component={Rooms}/>
+    <Route exact path="/rooms/:slug" component={SingleRoom}/>
+     
+   <Route component={Error}/>
+
+
+    </Switch>
+    
+  
+  </>
+
+ 
+  );
+}
+
+export default App;
---
+++ src/components/RoomContainer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import RoomsFilter from './RoomFilter';
+import RoomsList from './RoomList';
+import{RoomConsumer}from '../context';
+import Loading from './Loading'
+
+export default function RoomContainer() {
+    return (
+         <RoomConsumer>
+         {(value)=>{
+             const{loading,sortedRooms,rooms}=value
+
+
+             if(loading){return<Loading/>}
+
+             return ( 
+            <>
+            
+             <RoomsFilter rooms={rooms}/>
+             <RoomsList rooms={sortedRooms} />
+            </>
+            
+            
+
+         
+        );
+       }}
+     </RoomConsumer>
+        
+ );
+}
