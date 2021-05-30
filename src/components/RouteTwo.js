df62504d074425afc3724fa6a17004d972dadec0
Stefan <37850379+pomber12@users.noreply.github.com>
2021-05-30 21:12:04 +0300

    Add files via upload

---
+++ src/components/RouteTwo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+
+
+    return (
+         
+        <Route >
+        
+        <Redirect to="/Home" />
+        </Route>
+           
+              
+            
+        
+    );
+
+
+export default RouteTwo;
